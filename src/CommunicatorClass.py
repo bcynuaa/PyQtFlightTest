1f2e1f30ee34db23da2f78db5fe45fb5b100a72e
bcynuaa <bcynuaa@163.com>
2023-06-08 15:39:31 +0800

    before ui

--- src/CommunicatorClass.py
+++ src/CommunicatorClass.py
@@ -8,7 +8,7 @@
  # @ description: the class of communicator, which is a middle layer between the GUI and the backend
  '''
  
-from pyvistaqt import BackgroundPlotter, QtInteractor
+from pyvistaqt import QtInteractor, BackgroundPlotter
  
 from config.PyvistaSettings import *
 from config.MatplotlibSettings import *
@@ -23,13 +23,16 @@ class Communicator:
     
     def __init__(self) -> None:
         self.basic_magnification: float = 0.01
+        self.whether_database_loaded: bool = False
+        self.whether_domains_loaded: bool = False
+        self.whether_sensors_mode_dis_loaded: bool = False
         self.simulation_database: SimulationDatabase = SimulationDatabase()
         self.domains_with_gen_dis: DomainsWithGenDis = DomainsWithGenDis()
         self.sensors: Sensors = Sensors()
-        self.__initializePyvista()
+        self.__initializePyvistaPlotter()
         pass
     
-    def __initializePyvista(self) -> None:
+    def __initializePyvistaPlotter(self) -> None:
         self.flight_test_plotter: QtInteractor = QtInteractor()
         self.simulation_plotter: QtInteractor = QtInteractor()
         self.flight_test_plotter.add_mesh(pyvista.Sphere())
@@ -38,12 +41,31 @@ class Communicator:
     
     # ---------------------------------------------------------------------------------------------
     
+    def loadDatabaseFromPath(self, data_path: str) -> None:
+        self.simulation_database.loadDatabaseFromPath(data_path)
+        self.whether_database_loaded = True
+        pass
+    
+    def __refreshPyvistaPlotter(self) -> None:
+        self.flight_test_plotter.clear()
+        self.simulation_plotter.clear()
+        self.flight_test_plotter.add_mesh(self.flight_test_grid) # TODO: pyvista settings
+        self.simulation_plotter.add_mesh(self.simulation_grid) # TODO: pyvista settings
+        pass
+    
     def loadDomainFilesFromPath(self, data_path: str) -> None:
         self.domains_with_gen_dis.loadDomainFilesFromPath(data_path)
         self.flight_test_grid: pyvista.UnstructuredGrid = \
             self.domains_with_gen_dis.unstructured_grid.copy()
         self.simulation_grid: pyvista.UnstructuredGrid = \
             self.domains_with_gen_dis.unstructured_grid.copy()
+        self.__refreshPyvistaPlotter()
+        self.whether_domains_loaded = True
+        pass
+    
+    def loadSensorsModeDisFile(self, sensors_mode_dis_file: str) -> None:
+        self.sensors.loadSensorsModeDisFile(sensors_mode_dis_file)
+        self.whether_sensors_mode_dis_loaded = True
         pass
     
     # ---------------------------------------------------------------------------------------------
